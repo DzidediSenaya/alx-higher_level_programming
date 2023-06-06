78dacc3e447bb1bc967f427fd29f01ca3cd16ec7
Dzidedi Senaya <dzidedisenaya@gmail.com>
2023-06-06 19:28:01 +0000

    Update 100-print_tebahpla.py

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,4 +1,7 @@
 #!/usr/bin/python3
 
 for c in range(ord('z'), ord('A') - 1, -1):
-    print("{}".format(chr(c if c % 2 == 1 else c - 32)), end='')
+    if c % 2 == 0:
+        print("{:c}".format(c + 32), end='')
+    else:
+        print("{:c}".format(c), end='')
