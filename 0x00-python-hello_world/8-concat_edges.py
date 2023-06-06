a32f6e4460ff41b55688ec70b9458a8dc19e51ba
Dzidedi Senaya <dzidedisenaya@gmail.com>
2023-06-06 00:02:50 +0000

    Update 8-concat_edges.py

--- 0x00-python-hello_world/8-concat_edges.py
+++ 0x00-python-hello_world/8-concat_edges.py
@@ -1,3 +1,5 @@
+#!/usr/bin/python3
 str = "Python is an interpreted, interactive, object-oriented programming\
  language that combines remarkable power with very clear syntax"
-print(str[str.find("object"):str.find("object") + 25], end="\n")
+print(str[str.find("object"):str.find("object") + 25], end="")
+print(str[str.find("Python"):])
