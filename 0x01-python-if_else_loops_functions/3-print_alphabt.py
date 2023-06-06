e310f5f665b1795e0327acaa875a0126e96043b4
Dzidedi Senaya <dzidedisenaya@gmail.com>
2023-06-06 19:14:06 +0000

    modified to use only one print statement

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,8 +1,4 @@
 #!/usr/bin/python3
-
-output = ""
 for letter in range(97, 123):
-    if chr(letter) != 'q' and chr(letter) != 'e':
-        output += chr(letter)
-
-print(output, end="")
+if chr(letter) is not 'q' and chr(letter) is not 'e':
+print("{}".format(chr(letter)), end="")
