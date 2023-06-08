203edc19b7e47b46c339d4bbdc72d6b308968cf3
DzidediSenaya <dzidedisenaya@gmail.com>
2023-06-08 22:22:27 +0000

    finished all tasks

---
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+import dis
+
+def print_hidden_names():
+    with open("hidden_4.pyc", "rb") as file:
+        magic = file.read(4)
+        timestamp = file.read(4)
+        dis_header_size = file.read(4)
+
+        while True:
+            op = file.read(2)
+            if not op:
+                break
+
+            arg = file.read(4)
+            arg_value = int.from_bytes(arg, byteorder="little")
+
+            if op == b"\x01\x00":  # LOAD_CONST opcode
+                const = dis.get_instructions("", b"\x01\x00" + arg)
+                const_op = next(const)
+                const_value = const_op.argval
+                if isinstance(const_value, str) and not const_value.startswith("__"):
+                    print(const_value)
+
+print_hidden_names()
+
