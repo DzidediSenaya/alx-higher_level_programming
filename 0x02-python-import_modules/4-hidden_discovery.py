d1ba1c568484ff9ce2018e0e0cc435e1e2399d6d
Dzidedi Senaya <dzidedisenaya@gmail.com>
2023-06-09 00:50:41 +0000

    Update 4-hidden_discovery.py

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,10 +1,9 @@
-#!/usr/bin/python3
 import types
 import hidden_4
 
 if __name__ == "__main__":
     names = [name for name, value in vars(hidden_4).items()
-             if not name.startswith("__") and \
+             if not name.startswith("__") and 
              not isinstance(value, types.ModuleType)]
     for name in names:
         print(name)
