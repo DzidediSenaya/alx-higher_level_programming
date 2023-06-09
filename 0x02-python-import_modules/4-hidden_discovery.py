7c1826d6a66541db559a13fca54993cadec99346
Dzidedi Senaya <dzidedisenaya@gmail.com>
2023-06-09 00:52:23 +0000

    removed trailing whitespace in 4-hidden_discovery.py

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -3,7 +3,7 @@ import hidden_4
 
 if __name__ == "__main__":
     names = [name for name, value in vars(hidden_4).items()
-             if not name.startswith("__") and 
+             if not name.startswith("__") and
              not isinstance(value, types.ModuleType)]
     for name in names:
         print(name)
