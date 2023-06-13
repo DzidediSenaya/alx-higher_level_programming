242c15186035a6de1c6a2a18424a305a56cd0ca4
DzidediSenaya <dzidedisenaya@gmail.com>
2023-06-13 00:03:18 +0000

    modifying files to fit the requirements I overlooked

--- 0x03-python-data_structures/11-delete_at.py9-max_integer.py
+++ 0x03-python-data_structures/11-delete_at.py9-max_integer.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
+
 def delete_at(my_list=[], idx=0):
     if idx < 0 or idx >= len(my_list):
         return my_list
@@ -9,3 +10,9 @@ def delete_at(my_list=[], idx=0):
             new_list.append(my_list[i])
 
     return new_list
+
+my_list = [1, 2, 3, 4, 5]
+idx = 3
+new_list = delete_at(my_list, idx)
+print(new_list)
+print(my_list)
