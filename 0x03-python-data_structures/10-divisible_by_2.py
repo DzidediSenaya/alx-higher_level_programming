9053ced5251dedd829e1ebaebc4dc280dc9f8de3
DzidediSenaya <dzidedisenaya@gmail.com>
2023-06-13 00:25:46 +0000

    pycodestyle fixes

--- 0x03-python-data_structures/10-divisible_by_2.py
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -3,8 +3,10 @@
 def divisible_by_2(my_list=[]):
     return [num % 2 == 0 for num in my_list]
 
+
 my_list = [0, 1, 2, 3, 4, 5, 6]
 list_result = divisible_by_2(my_list)
+
 
 i = 0
 while i < len(list_result):
