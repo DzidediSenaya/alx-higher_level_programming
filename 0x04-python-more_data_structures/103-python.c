5f70710a38142de1c2b8b4e82a70cae61c70d003
DzidediSenaya <dzidedisenaya@gmail.com>
2023-06-14 23:20:30 +0000

    compiling task 16 with pyhton 3.4 sandbox

--- 0x04-python-more_data_structures/103-python.c
+++ 0x04-python-more_data_structures/103-python.c
@@ -1,4 +1,4 @@
-#include <python3.4m/Python.h>
+#include <Python.h>
 #include <stdio.h>
 
 void print_python_list(PyObject *p);
@@ -10,21 +10,28 @@ void print_python_bytes(PyObject *p);
  */
 void print_python_list(PyObject *p)
 {
-Py_ssize_t size, i;
-PyObject *item;
+	Py_ssize_t size, i;
+	PyObject *item;
 
-size = PyObject_Length(p);
-printf("[*] Python list info\n");
-printf("[*] Size of the Python List = %zd\n", size);
-printf("[*] Allocated = %zd\n", ((PyListObject *)p)->allocated);
+	size = ((PyVarObject *)p)->ob_size;
+	printf("[*] Python list info\n");
+	printf("[*] Size of the Python List = %zd\n", size);
 
-for (i = 0; i < size; i++)
-{
-item = PyList_GetItem(p, i);
-printf("Element %zd: %s\n", i, item->ob_type->tp_name);
-if (PyObject_IsInstance(item, (PyObject *)&PyBytes_Type))
-print_python_bytes(item);
-}
+	if (!PyList_Check(p))
+	{
+		printf("  [ERROR] Invalid List Object\n");
+		return;
+	}
+
+	printf("[*] Allocated = %zd\n", ((PyListObject *)p)->allocated);
+
+	for (i = 0; i < size; i++)
+	{
+		item = ((PyListObject *)p)->ob_item[i];
+		printf("Element %zd: %s\n", i, Py_TYPE(item)->tp_name);
+		if (PyBytes_Check(item))
+			print_python_bytes(item);
+	}
 }
 
 /**
@@ -33,26 +40,26 @@ print_python_bytes(item);
  */
 void print_python_bytes(PyObject *p)
 {
-Py_ssize_t size, i;
-char *str;
+	Py_ssize_t size, i;
+	char *str;
 
-printf("[.] bytes object info\n");
+	printf("[.] bytes object info\n");
 
-if (!PyObject_IsInstance(p, (PyObject *)&PyBytes_Type))
-{
-printf("  [ERROR] Invalid Bytes Object\n");
-return;
-}
+	if (!PyBytes_Check(p))
+	{
+		printf("  [ERROR] Invalid Bytes Object\n");
+		return;
+	}
 
-size = PyObject_Length(p);
-str = PyBytes_AsString(p);
+	size = ((PyVarObject *)p)->ob_size;
+	str = ((PyBytesObject *)p)->ob_sval;
 
-printf("  size: %zd\n", size);
-printf("  trying string: %s\n", str);
+	printf("  size: %zd\n", size);
+	printf("  trying string: %s\n", str);
 
-printf("  first %zd bytes:", size > 10 ? 10 : size);
-for (i = 0; i < size && i < 10; i++)
-printf(" %02hhx", str[i]);
-printf("\n");
+	printf("  first %zd bytes:", size + 1 > 10 ? 10 : size + 1);
+	for (i = 0; i < size + 1 && i < 10; i++)
+		printf(" %02hhx", str[i]);
+	printf("\n");
 }
 
