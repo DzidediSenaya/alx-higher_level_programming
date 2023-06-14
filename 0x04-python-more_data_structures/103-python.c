d3a8ed7d342f59f6c35eace3839cd4c36b74fc59
DzidediSenaya <dzidedisenaya@gmail.com>
2023-06-14 23:15:21 +0000

    compiling task 16 with pyhton 3.4 sandbox

--- 0x04-python-more_data_structures/103-python.c
+++ 0x04-python-more_data_structures/103-python.c
@@ -1,5 +1,8 @@
-#include <Python.h>
+#include <python3.4m/Python.h>
 #include <stdio.h>
+
+void print_python_list(PyObject *p);
+void print_python_bytes(PyObject *p);
 
 /**
  * print_python_list - Prints basic information about a Python list.
@@ -7,20 +10,21 @@
  */
 void print_python_list(PyObject *p)
 {
-	Py_ssize_t size, i;
-	PyObject *item;
+Py_ssize_t size, i;
+PyObject *item;
 
-	size = PyList_Size(p);
-	printf("[*] Python list info\n");
-	printf("[*] Size of the Python List = %ld\n", size);
+size = PyObject_Length(p);
+printf("[*] Python list info\n");
+printf("[*] Size of the Python List = %zd\n", size);
+printf("[*] Allocated = %zd\n", ((PyListObject *)p)->allocated);
 
-	printf("[*] Allocated = %ld\n", ((PyListObject *)p)->allocated);
-
-	for (i = 0; i < size; i++)
-	{
-		item = PyList_GetItem(p, i);
-		printf("Element %ld: %s\n", i, Py_TYPE(item)->tp_name);
-	}
+for (i = 0; i < size; i++)
+{
+item = PyList_GetItem(p, i);
+printf("Element %zd: %s\n", i, item->ob_type->tp_name);
+if (PyObject_IsInstance(item, (PyObject *)&PyBytes_Type))
+print_python_bytes(item);
+}
 }
 
 /**
@@ -29,27 +33,26 @@ void print_python_list(PyObject *p)
  */
 void print_python_bytes(PyObject *p)
 {
-	Py_ssize_t size, i;
-	Py_ssize_t max_print;
-	char *str;
+Py_ssize_t size, i;
+char *str;
 
-	printf("[.] bytes object info\n");
+printf("[.] bytes object info\n");
 
-	if (!PyBytes_Check(p))
-	{
-		printf("  [ERROR] Invalid Bytes Object\n");
-		return;
-	}
+if (!PyObject_IsInstance(p, (PyObject *)&PyBytes_Type))
+{
+printf("  [ERROR] Invalid Bytes Object\n");
+return;
+}
 
-	size = PyBytes_Size(p);
-	str = PyBytes_AsString(p);
+size = PyObject_Length(p);
+str = PyBytes_AsString(p);
 
-	printf("  size: %ld\n", size);
-	printf("  trying string: %s\n", str);
+printf("  size: %zd\n", size);
+printf("  trying string: %s\n", str);
 
-	printf("  first %ld bytes:", size > 10 ? 10 : size);
-	for (i = 0, max_print = (size > 10 ? 10 : size); i < max_print; i++)
-		printf(" %02x", (unsigned char)str[i]);
-	printf("\n");
+printf("  first %zd bytes:", size > 10 ? 10 : size);
+for (i = 0; i < size && i < 10; i++)
+printf(" %02hhx", str[i]);
+printf("\n");
 }
 
