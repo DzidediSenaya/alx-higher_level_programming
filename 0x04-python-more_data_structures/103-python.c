b63ad2942e32549e61e222425cc630c9e19d3e00
DzidediSenaya <dzidedisenaya@gmail.com>
2023-06-14 23:03:52 +0000

    compiling task 16 with python 3.4 sandbox

--- 0x04-python-more_data_structures/103-python.c
+++ 0x04-python-more_data_structures/103-python.c
@@ -1,10 +1,6 @@
 #include <Python.h>
 #include <stdio.h>
 
-
-void print_python_list(PyObject *p);
-void print_python_bytes(PyObject *p);
-
 /**
  * print_python_list - Prints basic information about a Python list.
  * @p: Pointer to the Python list object.
@@ -16,18 +12,16 @@ void print_python_list(PyObject *p)
 
 	size = PyList_Size(p);
 	printf("[*] Python list info\n");
-	printf("[*] Size of the Python List = %zd\n", size);
-	printf("[*] Allocated = %zd\n", ((PyListObject *)p)->allocated);
+	printf("[*] Size of the Python List = %ld\n", size);
+
+	printf("[*] Allocated = %ld\n", ((PyListObject *)p)->allocated);
 
 	for (i = 0; i < size; i++)
 	{
 		item = PyList_GetItem(p, i);
-		printf("Element %zd: %s\n", i, Py_TYPE(item)->tp_name);
-		if (PyBytes_Check(item))
-			print_python_bytes(item);
+		printf("Element %ld: %s\n", i, Py_TYPE(item)->tp_name);
 	}
 }
-
 
 /**
  * print_python_bytes - Prints basic information about a Python bytes object.
@@ -36,6 +30,7 @@ void print_python_list(PyObject *p)
 void print_python_bytes(PyObject *p)
 {
 	Py_ssize_t size, i;
+	Py_ssize_t max_print;
 	char *str;
 
 	printf("[.] bytes object info\n");
@@ -49,12 +44,12 @@ void print_python_bytes(PyObject *p)
 	size = PyBytes_Size(p);
 	str = PyBytes_AsString(p);
 
-	printf("  size: %zd\n", size);
-	printf("  trying string: %s\n", str ? str : "(unknown)");
+	printf("  size: %ld\n", size);
+	printf("  trying string: %s\n", str);
 
-	printf("  first %zd bytes:", size + 1 > 10 ? 10 : size + 1);
-	for (i = 0; i < size + 1 && i < 10; i++)
-		printf(" %02hhx", str[i]);
+	printf("  first %ld bytes:", size > 10 ? 10 : size);
+	for (i = 0, max_print = (size > 10 ? 10 : size); i < max_print; i++)
+		printf(" %02x", (unsigned char)str[i]);
 	printf("\n");
 }
 
