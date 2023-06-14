8d47416c949bc8e4aa5e39a8e1945838e27a1a49
Dzidedi Senaya <dzidedisenaya@gmail.com>
2023-06-14 22:54:17 +0000

    Update 1-search_replace.py

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -1,6 +1,4 @@
 #!/usr/bin/python3
-my_list = [1, 2, 3, 4, 5, 4, 2, 1, 1, 4, 89]
-new_list = search_replace(my_list, 2, 89)
-
-print(new_list)
-print(my_list)
+def search_replace(my_list, search, replace):
+    new_list = [replace if x == search else x for x in my_list]
+    return new_list
