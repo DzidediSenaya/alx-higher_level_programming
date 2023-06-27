ffacfdabd2579ef67ffcd091fa7e6c6bebde198b
Dzidedi Senaya <dzidedisenaya@gmail.com>
2023-06-27 23:25:03 +0000

    Update 1-safe_print_integer.py

--- 0x05-python-exceptions/1-safe_print_integer.py
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -1,19 +1,7 @@
 #!/usr/bin/python3
-def safe_print_list(my_list=[], x=0):
-    count = 0
+def safe_print_integer(value):
     try:
-        while count < x:
-            print("{:d}".format(my_list[count]), end=" ")
-            count += 1
-    except IndexError:
-        pass
-    finally:
-        print()
-    return count
-
-
-safe_print_list = __import__('0-safe_print_list').safe_print_list
-
-value = [89]
-nb_print = safe_print_list(value, len(value) + 2)
-print("Number of integers printed: {:d}".format(nb_print))
+        print("{:d}".format(value))
+        return True
+    except (ValueError, TypeError):
+        return False
