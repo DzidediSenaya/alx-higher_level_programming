6c7db21f65938ffc69ac4b468b20546bc8f2f797
Dzidedi Senaya <dzidedisenaya@gmail.com>
2023-07-03 21:49:49 +0000

    Update 0-rectangle.py

--- 0x08-python-more_classes/0-rectangle.py
+++ 0x08-python-more_classes/0-rectangle.py
@@ -1,3 +1,18 @@
 #!/usr/bin/python3
+"""
+Module: 0-rectangle
+Defines an empty class Rectangle that represents a rectangle.
+"""
+
 class Rectangle:
+    """
+    Rectangle class
+    """
+
     pass
+
+# Test the code
+if __name__ == "__main__":
+    my_rectangle = Rectangle()
+    print(type(my_rectangle))
+    print(my_rectangle.__dict__)
