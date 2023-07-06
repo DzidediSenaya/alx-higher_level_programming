7a767a46486371933b208708a82e4a06f95ee2dd
Dzidedi Senaya <dzidedisenaya@gmail.com>
2023-07-06 21:54:19 +0000

    Update 101-lazy_matrix_mul.py

--- 0x07-python-test_driven_development/101-lazy_matrix_mul.py
+++ 0x07-python-test_driven_development/101-lazy_matrix_mul.py
@@ -2,19 +2,20 @@
 """
 Module for lazy matrix multiplication using NumPy
 """
+
 import numpy as np
 
 
 def lazy_matrix_mul(m_a, m_b):
     """
-    Function that multiplies two matrices using NumPy
+    Function that multiplies two matrices using NumPy.
 
     Args:
         m_a (list): First matrix (list of lists)
         m_b (list): Second matrix (list of lists)
 
     Returns:
-        ndarray: Result of matrix multiplication
+        list: Result of matrix multiplication
 
     Raises:
         ValueError: If m_a or m_b is not a list or a list of lists
@@ -23,6 +24,7 @@ def lazy_matrix_mul(m_a, m_b):
         ValueError: If m_a or m_b is not a rectangle (rows have different sizes)
         ValueError: If m_a and m_b cannot be multiplied
     """
+
     if not isinstance(m_a, list) or not isinstance(m_b, list):
         raise ValueError("m_a and m_b must be a list")
 
@@ -39,7 +41,7 @@ def lazy_matrix_mul(m_a, m_b):
         raise ValueError("m_b should contain only integers or floats")
 
     if any(len(row) != len(m_a[0]) for row in m_a) or any(len(row) != len(m_b[0]) for row in m_b):
-        raise ValueError("each row of m_a must be of the same size" or "each row of m_b must be of the same size")
+        raise ValueError("Each row of m_a must be of the same size" or "Each row of m_b must be of the same size")
 
     if len(m_a[0]) != len(m_b):
         raise ValueError("m_a and m_b can't be multiplied")
