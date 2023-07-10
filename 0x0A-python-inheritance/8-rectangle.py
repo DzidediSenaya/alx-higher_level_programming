7103277dfbebd889b407ace73e7852afd301974b
Dzidedi Senaya <dzidedisenaya@gmail.com>
2023-07-10 22:46:33 +0000

    Update 8-rectangle.py

--- 0x0A-python-inheritance/8-rectangle.py
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -4,6 +4,29 @@ This module contains a rectangle class
 """
 
 
+class BaseGeometry:
+    """
+    A base class for geometry-related operations.
+    """
+
+    def integer_validator(self, name, value):
+        """
+        Validates that a value is an integer and greater than 0.
+
+        Args:
+            name (str): The name of the value.
+            value (int): The value to be validated.
+
+        Raises:
+            TypeError: If the value is not an integer.
+            ValueError: If the value is not greater than 0.
+        """
+        if not isinstance(value, int):
+            raise TypeError(f"{name} must be an integer")
+        if value <= 0:
+            raise ValueError(f"{name} must be greater than 0")
+
+
 class Rectangle(BaseGeometry):
     """
     Represents a rectangle.
@@ -11,8 +34,7 @@ class Rectangle(BaseGeometry):
     Inherits from BaseGeometry.
 
     Public Methods:
-        __init__(self, width, height): Initializes a rectangle with the
-        given width and height.
+        __init__(self, width, height): Initializes a rectangle with the given width and height.
     """
 
     def __init__(self, width, height):
