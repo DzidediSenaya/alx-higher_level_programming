4bbe1ba87f7f6bdf04b6e5ebc97b3869e316af16
Dzidedi Senaya <dzidedisenaya@gmail.com>
2023-07-11 18:24:16 +0000

    Update 7-add_item.py

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,23 +1,62 @@
 #!/usr/bin/python3
 
-"""Module contains Python script"""
+"""
+Module contains functions to save and load Python
+objects as JSON files.
+"""
 
 import sys
-from 5-save_to_json_file import save_to_json_file
-from 6-load_from_json_file import load_from_json_file
+from typing import Any
+from json import dump, load
 
-"""
-adds all arguments to a Python list,
-and then save them to a file
-"""
 
-filename = "add_item.json"
+def save_to_json_file(my_obj: Any, filename: str) -> None:
+    """
+    Writes a Python object to a JSON file.
 
-try:
-    my_list = load_from_json_file(filename)
-except FileNotFoundError:
-    my_list = []
+    Args:
+        my_obj (Any): The object to be saved as JSON.
+        filename (str): The name of the file to save the JSON
+        data.
 
-my_list.extend(sys.argv[1:])
+    Returns:
+        None
+    """
+    with open(filename, 'w') as file:
+        dump(my_obj, file)
 
-save_to_json_file(my_list, filename)
+
+def load_from_json_file(filename: str) -> Any:
+    """
+    Loads a JSON file and returns the corresponding Python object.
+
+    Args:
+        filename (str): The name of the JSON file to load.
+
+    Returns:
+        Any: The Python object loaded from the JSON file.
+    """
+    with open(filename) as file:
+        return load(file)
+
+
+def main() -> None:
+    """
+    Entry point of the script.
+    Reads command-line arguments, adds them to a Python list,
+    and saves the list as JSON in a file.
+    """
+    filename = "add_item.json"
+
+    try:
+        my_list = load_from_json_file(filename)
+    except FileNotFoundError:
+        my_list = []
+
+    my_list.extend(sys.argv[1:])
+
+    save_to_json_file(my_list, filename)
+
+
+if __name__ == "__main__":
+    main()
