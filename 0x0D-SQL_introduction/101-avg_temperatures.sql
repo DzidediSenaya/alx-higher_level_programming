03bc44ddfd7b2e401f300ca088e25cd8d198fa7f
Dzidedi Senaya <dzidedisenaya@gmail.com>
2023-08-16 00:45:40 +0000

    101-avg_temperatures.sql

--- 0x0D-SQL_introduction/101-avg_temperatures.sql
+++ 0x0D-SQL_introduction/101-avg_temperatures.sql
@@ -1,5 +1,4 @@
 -- Calculate the average temperature (Fahrenheit) by city ordered by temperature (descending)
-USE hbtn_0c_0;
 SELECT city, AVG(temperature) AS avg_temp
 FROM temperatures
 GROUP BY city
