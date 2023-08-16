b3b5b2b5207eb0657e7dddc1785accc521a940ff
Dzidedi Senaya <dzidedisenaya@gmail.com>
2023-08-16 00:46:48 +0000

    Update 102-top_city.sql

--- 0x0D-SQL_introduction/102-top_city.sql
+++ 0x0D-SQL_introduction/102-top_city.sql
@@ -1,5 +1,4 @@
 -- Calculate the average temperature during July and August for each city and display the top 3 cities ordered by temperature (descending)
-USE hbtn_0c_0;
 SELECT city, AVG(temperature) AS avg_temp
 FROM temperatures
 WHERE MONTH(date) IN (7, 8)
