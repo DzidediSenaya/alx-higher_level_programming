95b0deae756de9f3e02dbe0d016a6f0e7091bfd5
Dzidedi Senaya <dzidedisenaya@gmail.com>
2023-08-17 02:58:09 +0000

    Update 0-privileges.sql

--- 0x0E-SQL_more_queries/0-privileges.sql
+++ 0x0E-SQL_more_queries/0-privileges.sql
@@ -1,2 +1,5 @@
--- List all privileges for user_0d_1 and user_0d_2
-SHOW DATABASES;
+-- For user_0d_1
+SHOW GRANTS FOR 'user_0d_1'@'localhost';
+
+-- For user_0d_2
+SHOW GRANTS FOR 'user_0d_2'@'localhost';
