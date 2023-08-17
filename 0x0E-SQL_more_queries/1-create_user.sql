2c825451d4f3e2e00e17a204e22bf5d07e488df8
Dzidedi Senaya <dzidedisenaya@gmail.com>
2023-08-17 02:55:24 +0000

    Update 1-create_user.sql

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -1,5 +1,11 @@
--- creates the MySQL server user user_0d_1
--- Create MySQL user user_0d_1 if not exists
+-- Create user if not exists
 CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
-GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost';
+
+-- Grant all privileges to the user if the user exists
+GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost' WITH GRANT OPTION;
+
+-- If the user doesn't exist, this command will have no effect
+ALTER USER 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
+
+-- Flush privileges to apply changes
 FLUSH PRIVILEGES;
