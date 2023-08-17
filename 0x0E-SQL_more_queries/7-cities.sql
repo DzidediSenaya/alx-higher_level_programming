91387714397bc628c4232d94c726f28c3c57e696
Dzidedi Senaya <dzidedisenaya@gmail.com>
2023-08-17 03:16:55 +0000

    Update 7-cities.sql

--- 0x0E-SQL_more_queries/7-cities.sql
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -1,9 +1,6 @@
 -- Create database hbtn_0d_usa if not exists
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
 
--- Switch to the hbtn_0d_usa database
-USE hbtn_0d_usa;
-
 -- Create table cities if not exists
 CREATE TABLE IF NOT EXISTS cities (
   id INT AUTO_INCREMENT PRIMARY KEY,
