3e2d0c231b635b5b0d1099cc35a89b6347f1ac46
Dzidedi Senaya <dzidedisenaya@gmail.com>
2023-08-17 03:42:42 +0000

    reverse 7-cities.sql

--- 0x0E-SQL_more_queries/7-cities.sql
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -1,6 +1,9 @@
 -- Create database hbtn_0d_usa if not exists
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
 
+-- Switch to the hbtn_0d_usa database
+USE hbtn_0d_usa;
+
 -- Create table cities if not exists
 CREATE TABLE IF NOT EXISTS cities (
   id INT AUTO_INCREMENT PRIMARY KEY,
