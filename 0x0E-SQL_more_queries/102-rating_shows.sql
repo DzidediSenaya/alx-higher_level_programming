71c560189c07871b7e76535946a69ee990671a73
Dzidedi Senaya <dzidedisenaya@gmail.com>
2023-08-18 01:32:09 +0000

    Update 102-rating_shows.sql

--- 0x0E-SQL_more_queries/102-rating_shows.sql
+++ 0x0E-SQL_more_queries/102-rating_shows.sql
@@ -1,7 +1,9 @@
 -- Import the database hbtn_0d_tvshows_rate dump to your MySQL
-SELECT `title`, SUM(`rate`) AS `rating`
-FROM `tv_shows` AS `t`
+SELECT `name`, SUM(`rate`) AS `rating`
+FROM `tv_genres` AS `g`
+INNER JOIN `tv_show_genres` AS `s`
+ON `s`.`genre_id` = `g`.`id`
 INNER JOIN `tv_show_ratings` AS `r`
-ON `t`.`id` = `r`.`show_id`
-GROUP BY `title`
+ON `r`.`show_id` = `s`.`show_id`
+GROUP BY `name`
 ORDER BY `rating` DESC;
