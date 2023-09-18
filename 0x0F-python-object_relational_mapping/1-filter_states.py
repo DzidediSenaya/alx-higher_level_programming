7d29cf8a9c6b364c784181e518eeba4332e6ef73
Dzidedi Senaya <dzidedisenaya@gmail.com>
2023-09-18 15:56:48 +0000

    Update 1-filter_states.py

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -8,48 +8,33 @@ import sys
 
 
 if __name__ == "__main__":
-    # Check for the correct number of command line arguments
-    if len(sys.argv) != 4:
-        print("Usage: {} <username> <password> <database>".format(sys.argv[0]))
-        sys.exit(1)
-
+    # Get command line arguments
     username = sys.argv[1]
     password = sys.argv[2]
     database = sys.argv[3]
 
-    try:
-        # Connect to the MySQL server
-        db = MySQLdb.connect(
-            host="localhost",
-            port=3306,
-            user=username,
-            passwd=password,
-            db=database
-        )
+    # Connect to the MySQL server
+    db = MySQLdb.connect(
+        host="localhost",
+        port=3306,
+        user=username,
+        passwd=password,
+        db=database
+    )
 
-        # Create a cursor object to interact with the database
-        cursor = db.cursor()
+    # Create a cursor object to interact with the database
+    cursor = db.cursor()
 
-        # Execute the query to retrieve states starting with 'N' (uppercase)
-        query = (
-            "SELECT * FROM states "
-            "WHERE name LIKE 'N%' COLLATE utf8_bin "
-            "ORDER BY id"
-        )
-        cursor.execute(query)
+    # Execute the query to retrieve states starting with 'N'
+    cursor.execute("SELECT * FROM states WHERE name LIKE 'N%' ORDER BY id")
 
-        # Fetch all the results
-        states = cursor.fetchall()
+    # Fetch all the results
+    states = cursor.fetchall()
 
-        # Print the results
-        for state in states:
-            print(state)
+    # Print the results
+    for state in states:
+        print(state)
 
-    except MySQLdb.Error as e:
-        print("Error connecting to the database:", e)
-        sys.exit(1)
-
-    finally:
-        # Close cursor and database connections
-        cursor.close()
-        db.close()
+    # Close cursor and database connections
+    cursor.close()
+    db.close()
