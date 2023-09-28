a95fc5ed7310ca598f9f9590e179d7a85eafc983
Dzidedi Senaya <dzidedisenaya@gmail.com>
2023-09-28 16:14:28 +0000

    Update 1-body.sh

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Takes a URL as an argument, sends a GET request to the URL using curl, and displays the body of the response if the response status code is 200.
-curl -s -o /dev/stdout -w "%{http_code}" "$1" | grep -q 200 && curl -s "$1"
+curl -sL "$1"
