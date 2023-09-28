5a78bcb477a98c80a4ec933cf05acdc98b3df6da
Dzidedi Senaya <dzidedisenaya@gmail.com>
2023-09-28 16:29:14 +0000

    Update 102-catch_me.sh

--- 0x10-python-network_0/102-catch_me.sh
+++ 0x10-python-network_0/102-catch_me.sh
@@ -1,5 +1,3 @@
 #!/bin/bash
-# This script makes a request to 0.0.0.0:5000/catch_me that causes the server to respond with "You got me!".
-
-# Use curl to make a request with a custom header "Origin: HolbertonSchool" to trigger the desired response
-curl -sL -X PUT -H "Origin: HolbertonSchool" 0.0.0.0:5000/catch_me -d "user_id=98"
+# Sends a request to 0.0.0.0:5000/catch_me using curl to get the desired response
+curl -s -X PUT -d "user_id=98" -H "Origin: HolbertonSchool" 0.0.0.0:5000/catch_me
