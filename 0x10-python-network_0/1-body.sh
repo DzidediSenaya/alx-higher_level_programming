87ec5f6143632abd7672c67e77e714c51884594f
Dzidedi Senaya <dzidedisenaya@gmail.com>
2023-09-28 16:12:24 +0000

    Update 1-body.sh

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,26 +1,3 @@
 #!/bin/bash
-# This script takes a URL as an argument, sends a GET request to the URL using curl,
-# and displays the body of the response if the response status code is 200.
-
-# Check if a URL argument is provided
-if [ $# -ne 1 ]; then
-    echo "Usage: $0 <URL>"
-    exit 1
-fi
-
-# Get the URL from the command line argument
-url="$1"
-
-# Use curl to send a GET request and store the response in a variable
-response=$(curl -s -o /dev/stdout -w "%{http_code}" "$url")
-
-# Extract the status code from the response
-status_code=${response: -3}
-
-# Check if the status code is 200 (OK)
-if [ "$status_code" -eq 200 ]; then
-    # Display the body of the response
-    curl -s "$url"
-else
-    echo "Status code: $status_code"
-fi
+# Takes a URL as an argument, sends a GET request to the URL using curl, and displays the body of the response if the response status code is 200.
+curl -s -o /dev/stdout -w "%{http_code}" "$1" | grep -q 200 && curl -s "$1"
