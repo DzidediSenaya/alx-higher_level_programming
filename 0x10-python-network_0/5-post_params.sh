216330e4fa023fa2f6962cd56f969ae038eb2945
Dzidedi Senaya <dzidedisenaya@gmail.com>
2023-09-28 16:24:37 +0000

    Update 5-post_params.sh

--- 0x10-python-network_0/5-post_params.sh
+++ 0x10-python-network_0/5-post_params.sh
@@ -1,19 +1,3 @@
 #!/bin/bash
-# This script takes a URL as an argument, sends a POST request to the URL using curl
-# with specific parameters (email and subject), and displays the body of the response.
-
-# Check if a URL argument is provided
-if [ $# -ne 1 ]; then
-    echo "Usage: $0 <URL>"
-    exit 1
-fi
-
-# Get the URL from the command line argument
-url="$1"
-
-# Define the email and subject variables
-email="test@gmail.com"
-subject="I will always be here for PLD"
-
-# Use curl to send a POST request with specific parameters and display the body of the response
-curl -sX POST -d "email=$email" -d "subject=$subject" "$url"
+# Takes a URL as an argument, sends a POST request with specific variables using curl, and displays the body of the response.
+curl -s -X POST -d "email=test@gmail.com" -d "subject=I will always be here for PLD" "$1"
