371d80793c9b10ebfc5fe6e5eadb050e0053c855
Dzidedi Senaya <dzidedisenaya@gmail.com>
2023-10-24 21:55:29 +0000

    Update 100-starwars_characters.js

--- 0x14-javascript-web_scraping/100-starwars_characters.js
+++ 0x14-javascript-web_scraping/100-starwars_characters.js
@@ -1,16 +1,36 @@
 #!/usr/bin/node
 
 const request = require('request');
-const url = 'https://swapi.co/api/films/' + process.argv[2];
-request(url, function (error, response, body) {
-  if (!error) {
-    const characters = JSON.parse(body).characters;
-    characters.forEach((character) => {
-      request(character, function (error, response, body) {
-        if (!error) {
-          console.log(JSON.parse(body).name);
+const movieId = process.argv[2];
+
+const apiUrl = `https://swapi-api.alx-tools.com/api/films/${movieId}`;
+
+request(apiUrl, (error, response, body) => {
+  if (error) {
+    console.error(error);
+  } else if (response.statusCode === 200) {
+    const movieData = JSON.parse(body);
+    console.log("Characters in " + movieData.title + ":");
+
+    const characterUrls = movieData.characters;
+
+    const fetchCharacterName = (url) => {
+      request(url, (charError, charResponse, charBody) => {
+        if (charError) {
+          console.error(charError);
+        } else if (charResponse.statusCode === 200) {
+          const characterData = JSON.parse(charBody);
+          console.log(characterData.name);
+        } else {
+          console.error("Error fetching character data. Status code: " + charResponse.statusCode);
         }
       });
+    };
+
+    characterUrls.forEach((url) => {
+      fetchCharacterName(url);
     });
+  } else {
+    console.error("Error fetching movie data. Status code: " + response.statusCode);
   }
 });
